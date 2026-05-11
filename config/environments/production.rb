6c5ac3b7bef768bdaf33aadf26609fa3b623bd8f
EvilivE123 <palande.gaurav777@gmail.com>
2026-05-11 08:46:02 +0000

    adding host in production.rb V3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-
+  config.action_dispatch.trusted_proxies = [IPAddr.new("0.0.0.0/0"), IPAddr.new("::/0")]
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
